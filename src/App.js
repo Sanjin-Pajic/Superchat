a4b90d60944653c362e9564056f9d33a2ada7c0b
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-07-25 20:34:44 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>⚛️🔥💬</h1>
+        <h1 className="superchat">⚛️ superchat </h1>
         <SignOut />
       </header>
 
@@ -49,7 +49,6 @@ function SignIn() {
   return (
     <>
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
   )
 
