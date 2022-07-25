c17dd8879f4a84da345598b07d7398fa1407081c
Sanjin-Pajic <pajicsanjo@gmail.com>
2022-07-25 12:53:40 +0200

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import firebase from 'firebase/compat/app'; 
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth'; 
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAKwvYd44Gc7IA3F3S3JfqtMiBo3siqEHk",
+    authDomain: "superchat-1eeb1.firebaseapp.com",
+    projectId: "superchat-1eeb1",
+    storageBucket: "superchat-1eeb1.appspot.com",
+    messagingSenderId: "992887406252",
+    appId: "1:992887406252:web:53eac974460c090c83e909",
+    measurementId: "G-M4QG7YELMV"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
   return (
-  <div>Hi</div>
+  <div className='App'>
+    <header className='App-header'>
+
+    </header>
+  </div>
   );
 }
 
